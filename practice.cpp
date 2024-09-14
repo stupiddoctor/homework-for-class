edd963fb5ce5c535dafc5a462cb5097ef5a53602
stupiddoctor <1961543269@qq.com>
2024-09-14 10:05:50 +0800

    创建目录

--- practice.cpp
+++ practice.cpp
@@ -1,20 +1,7 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-/*int main()
-{int x,y,sum;
-x=5;
-y=9;
-int*p=&x;
-cout<<&x<<'\n';
-cout<<&*p<<'\n';
-cout<<p<<'\n';
-cout<<*p<<'\n';
-x=29;
-cout<<&x;
-return 0;
-}*/
-//about the address
+
 class base
 {
 	public:
